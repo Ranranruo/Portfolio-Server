b6c290e6bb4c4c7883f93fe61a442b859f0f84ed
Ranranruo <tlsalstjr58@gmail.com>
2024-06-06 15:16:32 +0900

    first commit

---
+++ src/main/java/minseok/Portfolio_Server/PortfolioServerApplication.java
@@ -0,0 +1,13 @@
+package minseok.Portfolio_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/minseok/Portfolio_Server/PortfolioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package minseok.Portfolio_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
